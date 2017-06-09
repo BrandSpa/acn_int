b7e406c30caba4f67f02595fd080b12b0bbfb2e6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-09 12:08:43 -0500

    return content

--- apis/posts.php
+++ apis/posts.php
@@ -12,7 +12,7 @@ function bs_get_posts($type = array('video','gallery','featured','post'), $paged
 	$posts = array_map(function($post) {
 			$thumbId = get_post_thumbnail_id($post->ID);
 			$post->post_image = get_post_meta($post->ID, 'image_square_key') ? str_replace('http:', '', get_post_meta($post->ID, 'image_square_key')) : '';
-			$content = substr($post->post_content, 0, 250) ? substr($post->post_content, 0, 250) : '';
+			$content = substr($post->post_content, 0, 250) ? substr($post->post_content, 0, 250) : $post->post_content;
 			$post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags($content, 0, 250) );
 			$post->post_permalink = get_post_permalink($post->ID);
 			return $post;
