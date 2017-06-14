fed26fb39ecac347556d747a030052cb0cf41ce7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-14 17:14:57 -0500

    fix image square api

--- apis/posts.php
+++ apis/posts.php
@@ -11,7 +11,8 @@ function bs_get_posts($type = array('video','gallery','featured','post'), $paged
 
 	$posts = array_map(function($post) {
 			$thumbId = get_post_thumbnail_id($post->ID);
-			$post->post_image = get_post_meta($post->ID, 'image_square_key') ? str_replace('http:', '', get_post_meta($post->ID, 'image_square_key')) : '';
+			$image  = strpos(get_post_meta($post->ID, 'image_square_key'), 'http:') ? str_replace('http:', '', get_post_meta($post->ID, 'image_square_key')) : get_post_meta($post->ID, 'image_square_key');
+			$post->post_image = $image;
 			$content = substr($post->post_content, 0, 250) ? substr($post->post_content, 0, 250) : $post->post_content;
 			$post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags($content, 0, 250) );
 			$post->post_permalink = get_post_permalink($post->ID);
