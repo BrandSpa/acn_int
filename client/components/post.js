87ff2403289973401e4cba751b41900c8e0e0f23
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-15 10:00:56 -0500

    fix error img reference

--- client/components/post.js
+++ client/components/post.js
@@ -27,6 +27,7 @@ class Post extends React.Component {
     const { post, type, read_more } = this.props;
     const { post_image = '', post_title = '' } = post;
     let title = post_title;
+
     if (window.innerWidth <= "767") {
       title = post_title.substring(0, 70) + "...";
     }
@@ -49,13 +50,14 @@ class Post extends React.Component {
               : <div/>
             }
           </a>
+
           <div
             className={
               type == "main"
                 ? "grid-item__content__texts grid-item--main__content__texts"
                 : "grid-item__content__texts"
             }
-            style={img == "" ? { width: "100%" } : {}}
+            style={!post_image.length > 0 ? { width: "100%" } : {}}
           >
             <h5><a href={post.post_permalink}>{title}</a></h5>
             <p>{`${post.post_short.substring(0, 220)}...`}</p>
--- footer-donate.php
+++ footer-donate.php
@@ -25,8 +25,8 @@
   bs.donate = '<?php echo gett('Donate') ?>';
 </script>
 
-<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.86294264bd65d9458470.js'></script>
-<script src='<?php echo get_template_directory_uri() ?>/public/js/app.7488fba418a40b9f17aa.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.e4a5cda070be85caec77.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/app.9370c56b5074a3e907fa.js'></script>
 
 <!--/app theme-->
 
