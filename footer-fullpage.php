c86cda0d82da7ae5f752c7a421c866391f5e790d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-27 14:01:09 -0500

    add global bs

--- footer-fullpage.php
+++ footer-fullpage.php
@@ -1,3 +1,8 @@
+<script>
+  var bs = {};
+  bs.lang = '<?php echo getCountryLang(getCountry()) ?>';
+  bs.donate = '<?php echo gett('Donate') ?>';
+</script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.js"></script>
