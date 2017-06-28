8d6fced1d7889d1fc3dd201303c8c8fd2b6cb147
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-28 11:08:11 -0500

    add bg mobile

--- shortcodes/fullpage/fullpage_slide.php
+++ shortcodes/fullpage/fullpage_slide.php
@@ -3,11 +3,13 @@
 function acn_fullpage_slide_sc( $atts, $content ) {
 	$at = shortcode_atts([
 		"bg_img" => "",
+		"bg_img_mobile" => "",
 		"bg_color" => "#fff",
 		"uniq_name" => "slide-" . uniqid() . rand(0, 100) 
 	], $atts);
 	
 	$bgUrl = wp_get_attachment_url( $at['bg_img'] ); 
+	$$bgUrlMobile = wp_get_attachment_url( $at['bg_img_mobile'] ); 
 	ob_start();
 	?>
 		<div
@@ -15,7 +17,7 @@ function acn_fullpage_slide_sc( $atts, $content ) {
 			data-anchor="<?php echo $at['uniq_name'] ?>"
 			class="section lazyload" 
 			data-src="<?php echo $bgUrl ?>"
-			data-bgset="<?php echo $bgUrl ?> 1200w"
+			data-bgset="<?php echo $bgUrl ?> 1200w, <?php echo $bgUrlMobile ?> 750w"
 			style="background-size: cover; background-position: center center"
 		>
 			<?php echo do_shortcode($content) ?>
