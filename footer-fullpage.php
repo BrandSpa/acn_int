fc80a92cda590c78ef46bc7def89390ad401beec
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-28 15:38:50 -0500

    add data sizes

--- footer-fullpage.php
+++ footer-fullpage.php
@@ -6,8 +6,8 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/3.0.0/lazysizes.min.js"></script>
 <script src="https://afarkas.github.io/lazysizes/plugins/bgset/ls.bgset.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/3.0.0/lazysizes.min.js"></script>
 
 <script src='<?php echo get_template_directory_uri() ?>/client/dist/vendor.d8f3f7efe5e0dfb79a34.js'></script>
 <script src='<?php echo get_template_directory_uri() ?>/client/dist/app.326d5fe03a2a4ae7a34f.js'></script>
