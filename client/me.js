8118181c19da7d6dd59cc738cfee55c053e46285
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-28 18:38:13 -0500

    change

--- client/me.js
+++ client/me.js
@@ -2,18 +2,5 @@ import "fullpage.js";
 import "lazysizes";
 
 $(function() {
-  $(document).on("click", ".section__open-post", function(e) {
-    var $postSection = $(this).closest(".fp-tableCell").find(".section__post");
-    if ($postSection.hasClass("section__post--open")) {
-      $.fn.fullpage.setAllowScrolling(true);
-      $postSection.removeClass("section__post--open");
-    } else {
-      $.fn.fullpage.setAllowScrolling(false);
-      $postSection.addClass("section__post--open");
-    }
-  });
-
-  $(document).on("click", ".section__down", function() {
-    $.fn.fullpage.moveSectionDown();
-  });
+ 
 });
