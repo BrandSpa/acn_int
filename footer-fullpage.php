7f2272bcb16424518c489200125a66d2131f5a2b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-28 15:51:27 -0500

    add parent fit

--- footer-fullpage.php
+++ footer-fullpage.php
@@ -6,6 +6,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.js"></script>
+<script src="https://afarkas.github.io/lazysizes/plugins/bgset/ls.parent-fit.min.js"></script>
 <script src="https://afarkas.github.io/lazysizes/plugins/bgset/ls.bgset.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/3.0.0/lazysizes.min.js"></script>
 
