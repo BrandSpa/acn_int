da261e459b490c7e13b4c87ac9aeda95a11adee4
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-29 11:26:20 -0500

    change bg

--- shortcodes/fullpage/fullpage.php
+++ shortcodes/fullpage/fullpage.php
@@ -53,29 +53,33 @@ function acn_fullpage_sc( $atts, $content ) {
 					}
 				});
 
-
-				$(document).on("click", ".section__open-post", function(e) {
-					var $postSection = $(this).closest(".fp-tableCell").find(".section__post");
-					if ($postSection.hasClass("section__post--open")) {
+				function closePost() {
 						$.fn.fullpage.setAllowScrolling(true);
 						$postSection.removeClass("section__post--open");
 						$(this).find('i').removeClass('ion-close-round');
 						$(this).find('i').addClass('ion-android-open');
+				}
+
+				function openPost() {
+					$.fn.fullpage.setAllowScrolling(false);
+					$postSection.addClass("section__post--open");
+					$(this).find('i').removeClass('ion-android-open');
+					$(this).find('i').addClass('ion-close-round');
+				}
+
+				$(document).on("click", ".section__open-post", function handleTogglePost(e) {
+					var $postSection = $(this).closest(".fp-tableCell").find(".section__post");
+
+					if ($postSection.hasClass("section__post--open")) {
+						closePost();
 					} else {
-						$.fn.fullpage.setAllowScrolling(false);
-						$postSection.addClass("section__post--open");
-						$(this).find('i').removeClass('ion-android-open');
-						$(this).find('i').addClass('ion-close-round');
-						
+						openPost();
 					}
 				});
 
 				$(document).on("click", ".section__down", function() {
 					$.fn.fullpage.moveSectionDown();
 				});
-
-		
-
 			});	
 		</script>
 	<?php
--- shortcodes/fullpage/fullpage_slide.php
+++ shortcodes/fullpage/fullpage_slide.php
@@ -16,24 +16,14 @@ function acn_fullpage_slide_sc( $atts, $content ) {
 		<div
 			data-anchor="<?php echo $at['uniq_name'] ?>"
 			class="section section--<?php echo $at['uniq_name'] ?>" 
+			data-bgset="<?php echo $bgUrl ?> 1200w, <?php echo $bgUrlMobile ?> 600w"
 			style="background-size: cover; background-position: center center"
 		>
+		
 		<style>
-
 			.section--<?php echo $at['uniq_name'] ?> {
 				background-color: #000;
-			}
-
-			.section--<?php echo $at['uniq_name'] ?>.load-bgs {
-				background-image: url(<?php echo $bgUrl ?>);
-			}
-
-			@media (max-width: 767px) {
-				.section--<?php echo $at['uniq_name'] ?>.load-bgs {
-					background-image: url(<?php echo $bgUrlMobile ?>)
-				}
-			}
-		
+			}		
 		</style>
 			<div class="section__content">
 				<?php echo do_shortcode($content) ?>
