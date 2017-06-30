5a5f799d73b17b85270351e4bfbfc559af56ca46
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-30 16:19:47 -0500

    improve num

--- shortcodes/fullpage/fullpage.php
+++ shortcodes/fullpage/fullpage.php
@@ -23,7 +23,7 @@ function acn_fullpage_sc( $atts, $content ) {
 	<div class="fullpage-nav">
 		<ul>
 		<?php foreach($links  as $ind => $link): ?>
-			<li><a href="<?php echo  $link['link'] ?>"><span class="num"><?php echo $ind ?></span> <?php echo  $link['title'] ?></a></li>
+			<li><a href="<?php echo $link['link'] + 1 ?>"><span class="num"><?php echo $ind ?></span> <?php echo  $link['title'] ?></a></li>
 		<?php endforeach; ?>
 		</ul>
 	</div>
