622f76166f42eb692d57ef7ece2bfe53bdea4701
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 10:44:39 -0500

    clean content

--- shortcodes/posts.php
+++ shortcodes/posts.php
@@ -14,6 +14,7 @@ function bs_posts_sc($atts, $content = null) {
 	$posts = array_map(function($post) {
 			$post->post_image = str_replace('http:', '', get_post_meta($post->ID, 'image_square_key', true));
 			$content = substr($post->post_content, 0, 250) ? substr($post->post_content, 0, 250) : $post->post_content;
+			$post->post_content = '';
 			$post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags($content, 0, 250) );
 			$post->post_permalink = get_post_permalink($post->ID);
 			return $post;
