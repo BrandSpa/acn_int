d15ae16bb1480bc417fc18dcc38d631281ac943f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 10:43:34 -0500

    fix route

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 		<!-- /wordpress files-->
 
 
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/client/dist/app.cssv=<?php echo filemtime(get_template_directory() . '/client/dist/app.css') ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/client/dist/app.css?v=<?php echo filemtime(get_template_directory() . '/client/dist/app.css') ?>">
 	<link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
 	<script>
