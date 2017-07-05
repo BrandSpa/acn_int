a4c507b38b06d64dabb7f7d095c02551c01b75ce
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 10:56:29 -0500

    change post short

--- apis/posts.php
+++ apis/posts.php
@@ -13,7 +13,7 @@ function bs_get_posts($type = array('video','gallery','featured','post'), $paged
 			$images = !empty(get_post_meta($post->ID, 'image_square_key', true)) ? get_post_meta($post->ID, 'image_square_key', true) : '';
  			$post->post_image = str_replace('http:', '', $images);
 			$content = substr($post->post_content, 0, 250) ? substr($post->post_content, 0, 250) : $post->post_content;
-			// $post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags($content, 0, 250) );
+			$post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags(substr($post->post_content, 0, 200)) );
 			$post->post_permalink = get_post_permalink($post->ID);
 			return $post;
 	}, $query->get_posts());
