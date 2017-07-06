5a163d2d4696aee4707561f07fed5c7a8534e386
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-06 11:38:27 -0500

    fix assets footer fullpage

--- footer-fullpage.php
+++ footer-fullpage.php
@@ -4,19 +4,17 @@
   bs.donate = '<?php echo gett('Donate') ?>';
 </script>
 
+<script src="<?php echo get_template_directory_uri() ?>/client/dist/vendor.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/vendor.js') ?>"></script>
+<script src="<?php echo get_template_directory_uri() ?>/client/dist/app.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/app.js') ?>
+"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://afarkas.github.io/lazysizes/plugins/bgset/ls.bgset.min.js"></script>
 
 <script
-  src="<?php echo get_template_directory_uri() ?>/client/me/vendor.aebf259c4a2c6937c4c6.js') ?>"
->
-</script>
-
+  src="<?php echo get_template_directory_uri() ?>/client/me/vendor_me.js?v=<?php echo filemtime(get_template_directory() . '/client/me/vendor_me.js') ?>"></script>
 <script
-  src="<?php echo get_template_directory_uri() ?>/client/me/app.78ef1ef404c7ff8af8dd.js') ?>">
-</script>
+  src="<?php echo get_template_directory_uri() ?>/client/me/app_me.js?v=<?php echo filemtime(get_template_directory() . '/client/me/app_me.js') ?>"></script>
 
-<script src='<?php echo get_template_directory_uri() ?>/client/dist/app.23966adf2dc9bac8c927.js'></script>
-<script src='<?php echo get_template_directory_uri() ?>/client/dist/app.23966adf2dc9bac8c927.js'></script>
 </body>
 </html>
