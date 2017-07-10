ecd9fee4b24e497e304deb15b3eac83643ab9059
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-10 12:00:24 -0500

    num to string

--- client/components/counter.js
+++ client/components/counter.js
@@ -19,10 +19,9 @@ class Counter extends Component {
     const { time, delay } = this.props;
     let nums = [];
     let total = time / delay;
-    let $el = $('.counter');
     let num = this.props.num;
     let isFloat = /^[0-9]+\.[0-9]+$/.test(num);
-    let decimalLength = isFloat ? (num.split('.')[1] || []).length : 0;
+    let decimalLength = isFloat ? (num.toString().split('.')[1] || []).length : 0;
     num = parseInt(num);
 
     for (var i = 0; i <= total; i++) {
