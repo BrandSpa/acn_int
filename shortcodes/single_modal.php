221bf3e4c8d448337e59e7cb8e914718cd4baa8d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-10 09:39:21 -0500

    fix wp get attachment url

--- shortcodes/single_modal.php
+++ shortcodes/single_modal.php
@@ -11,16 +11,18 @@ function bs_single_modal_sc($atts, $content = null) {
   ob_start();
 ?>
 
-<?php 
- 
+<?php
+
 ?>
 
 <!-- Place somewhere in the <body> of your page -->
 <!--<link href="<?php //echo get_template_directory_uri() ?>/public/css/lightbox.css" rel="stylesheet">-->
 <div class="single_modal" style="margin:20px 0;">
-		<?php $image = $at['image'];//foreach(explode(',', $at['images']) as $image): ?>
-        <?php $attachment_meta = wp_get_attachment($image); ?>
-		 
+		<?php
+				$image = $at['image'];
+        $attachment_meta = wp_get_attachment_url($image);
+			?>
+
       <a href="<?php echo wp_get_attachment_url($image) ?>" rel="lightbox['sameGroup']" title="<?php echo $attachment_meta['description']; ?>">
         <img style="max-width:100%;" src="<?php echo wp_get_attachment_url($image) ?>" alt="<?php echo $attachment_meta['alt']; ?>" />
         <!--<div class="modal_text" style="position:absolute; top:15px; padding:20px; color:#FFF;"><h5>+ Photo Gallery</h5></div>-->
@@ -50,7 +52,7 @@ add_action( 'vc_before_init', 'bs_single_modal_vc' );
         "param_name" => "image",
         "value" => ''
 			],
- 
+
 		];
 
   	vc_map(
@@ -59,7 +61,6 @@ add_action( 'vc_before_init', 'bs_single_modal_vc' );
         "base" => "bs_single_modal",// igual a add_shortcode( 'bs_flexslider', 'bs_example_sc' );
         "category" =>  "BS",
         "params" => $params
-      ) 
+      )
     );
   }
-
