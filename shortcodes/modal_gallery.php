9661c6b8f243b5901457f0862c4c2326da0f9e4e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-10 16:25:15 -0500

    add data title

--- shortcodes/modal_gallery.php
+++ shortcodes/modal_gallery.php
@@ -23,14 +23,21 @@ function bs_modal_gallery_sc($atts, $content = null) {
     <?php $countmodal=0;?>
 		<?php foreach(explode(',', $at['images']) as $image): ?>
 
-      <a href="<?php echo wp_get_attachment_url($image) ?>" data-lightbox="<?php echo $at['groupname'] ?>">
-      <?php if($countmodal==0)
-      {?>
-        <img style="max-width:100%;" src="<?php echo wp_get_attachment_url($image) ?>" alt="<?php echo get_post_meta($image, '_wp_attachment_image_alt', true) ?>" />
+      <a
+				href="<?php echo wp_get_attachment_url($image) ?>"
+				data-lightbox="<?php echo $at['groupname'] ?>"
+				data-title="<?php echo get_post_meta($image, '_wp_attachment_image_alt', true) ?>"
+			>
+      <?php if($countmodal == 0): ?>
+        <img
+					style="max-width:100%;"
+					src="<?php echo wp_get_attachment_url($image) ?>"
+					alt="<?php echo get_post_meta($image, '_wp_attachment_image_alt', true) ?>"
+				/>
         <div class="modal_text" style="position:absolute; top:15px; padding:20px; color:#FFF;"><h5>+ Photo Gallery</h5></div>
-      <?php }else{?>
+      <?php else: ?>
         <img style="display:none;" src="<?php echo wp_get_attachment_url($image) ?>" alt="<?php echo get_post_meta($image, '_wp_attachment_image_alt', true) ?>"  />
-      <?php }?>
+      <?php endif; ?>
       </a>
 		<?php
     $countmodal++;
