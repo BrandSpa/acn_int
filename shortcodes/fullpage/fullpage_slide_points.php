dddd1917f1cb6dd0e988d3af2662d975a4b64bbd
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-11 16:17:46 -0500

    change position content

--- shortcodes/fullpage/fullpage_slide_points.php
+++ shortcodes/fullpage/fullpage_slide_points.php
@@ -26,6 +26,7 @@ function acn_fullpage_slide_points_sc( $atts, $content ) {
 
 	<div class="section__content">
 		<?php echo $content ?>
+	</div>
 		<div class="map-points__container">
 			<svg class="map-points" width="1409" height="695" preserveAspectRatio="xMidYMid slice" viewBox="0 0 1920 1080">
 				<image class="hotspot__bg-image" width="1920" height="1080"
@@ -186,7 +187,7 @@ function acn_fullpage_slide_points_sc( $atts, $content ) {
 
 </svg>
 </div>
-</div>
+
 
 </div>
 
