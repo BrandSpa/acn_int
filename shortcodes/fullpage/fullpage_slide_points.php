982c064a0b35b9b3f141f499b3ab87524808e1ba
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-11 16:26:19 -0500

    do shortcode

--- shortcodes/fullpage/fullpage_slide_points.php
+++ shortcodes/fullpage/fullpage_slide_points.php
@@ -25,8 +25,9 @@ function acn_fullpage_slide_points_sc( $atts, $content ) {
 	>
 
 	<div class="section__content">
-		<?php echo $content ?>
+		<?php echo do_shortcode($content) ?>
 	</div>
+
 		<div class="map-points__container">
 			<svg class="map-points" width="1409" height="695" preserveAspectRatio="xMidYMid slice" viewBox="0 0 1920 1080">
 				<image class="hotspot__bg-image" width="1920" height="1080"
