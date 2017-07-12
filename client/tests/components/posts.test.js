e8c559bd8204152c52c2ecbd9afc0e2eec7e9d36
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-12 11:06:12 -0500

    change line-height p

--- client/tests/components/posts.test.js
+++ client/tests/components/posts.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-import moxios from 'moxios';
+import moxios from "moxios";
 import Posts from "../../components/posts";
-import axios from 'axios';
-import sinon from 'sinon';
+import axios from "axios";
+import sinon from "sinon";
 
-describe('Posts component', () => {
+describe("Posts component", () => {
 	beforeEach(() => {
 		moxios.install();
 	})
@@ -14,16 +14,16 @@ describe('Posts component', () => {
 		moxios.uninstall();
 	})
 
-	it('should show 3 posts', () => {
+	it("should show 3 posts", () => {
 		let posts = [{id: 1}, {id: 2}, {id: 3}];
 		let wrapper = mount(<Posts posts={posts} />);
-		expect(wrapper.find('.grid-item').length).toBe(3);
+		expect(wrapper.find(".grid-item").length).toBe(3);
 	})
 
-	it('should get posts on componentWillMount', () => {
+	it("should get posts on componentWillMount", () => {
 		let posts = [{id: 1}, {id: 2}, {id: 3}];
 		let wrapper = mount(<Posts posts={posts} />);
 		expect(wrapper.props().posts).toEqual(posts);
-		expect(wrapper.find('.grid-item').length).toBe(3);
+		expect(wrapper.find(".grid-item").length).toBe(3);
 	})
 })
