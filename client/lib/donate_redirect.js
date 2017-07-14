78a8d7ef19a232b1d994e1671e3c5d41011425fc
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-14 15:23:08 -0500

    check first for ga

--- client/lib/donate_redirect.js
+++ client/lib/donate_redirect.js
@@ -9,11 +9,10 @@ export default () => {
   if($('.bs-donate')) {
     $('.bs-donate').on('click', e => {
       e.preventDefault();
-      if (ga)
-        ga('send', 'event', 'DONATION', 'DONATION_CLICK', 'DONATION_CLICK', 0);
+      if(ga) ga('send', 'event', 'DONATION', 'DONATION_CLICK', 'DONATION_CLICK', 0);
 
       let data = qs.stringify({action: 'donate_redirect'});
-
+      
       request
         .post('/wp-admin/admin-ajax.php', data)
         .then(res => window.location = res.data);
