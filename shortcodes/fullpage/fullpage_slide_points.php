a591d747739661e7237999f04ef21ff9715265a7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-14 20:58:08 -0500

    add validations

--- shortcodes/fullpage/fullpage_slide_points.php
+++ shortcodes/fullpage/fullpage_slide_points.php
@@ -42,41 +42,41 @@ function acn_fullpage_slide_points_sc( $atts, $content ) {
 
 					  <div class="col-4-l" style="padding-bottom: 40px">
 					    <div style="margin-bottom: 40px">
-					      <h3><?php echo $cont['families_already_returned_num'] ?></h3>
+					      <h3><?php echo isset($cont['families_already_returned_num']) ? $cont['families_already_returned_num'] : '' ?></h3>
 					      <p><?php echo $cont['families_already_returned_text'] ?></p>
 					    </div>
 
 							<div style="margin-bottom: 40px">
-					      <h3 style="color: red;font-weight: 500;"><?php echo $cont['damaged_houses_num'] ?></h3>
+					      <h3 style="color: red;font-weight: 500;"><?php echo isset($cont['damaged_houses_num']) ?  $cont['damaged_houses_num'] : ''?></h3>
 					      <h3 style="color: red;font-weight: 500;"><?php echo $cont['damaged_houses_title_1'] ?></h3>
 					      <h3 style="color: red;font-weight: 500;"><?php echo $cont['damaged_houses_title_2'] ?></h3>
 					    </div>
 
 							<div style="margin-bottom: 40px">
-					    	<h1 style="font-weight: 700;"><?php echo $cont['percentage'] ?></h1>
+					    	<h1 style="font-weight: 700;"><?php echo isset($cont['percentage']) ? $cont['percentage'] : '' ?></h1>
 					    </div>
 
-					    <h4 style="color: red;"><?php echo $cont['number_of_houses_registered_to_be_renovated_num'] ?></h4>
+					    <h4 style="color: red;"><?php echo isset($cont['number_of_houses_registered_to_be_renovated_num']) ? $cont['number_of_houses_registered_to_be_renovated_num'] : ''  ?></h4>
 					    <p><?php echo $cont['number_of_houses_registered_to_be_renovated_text'] ?></p>
 					  </div>
 
 					  <div class="col-4-l" style="padding-bottom: 40px">
 					    <div style="margin-bottom: 140px">
-						    <h3><?php echo $cont['christians_already_returned_num'] ?></h3>
+						    <h3><?php echo isset($cont['christians_already_returned_num']) ? $cont['christians_already_returned_num'] : ''?></h3>
 						    <p><?php echo $cont['christians_already_returned_text'] ?></p>
 					    </div>
 
 					    <ul style="margin-bottom: 40px">
 					      <li style="margin-bottom: 20px">
-					        <h4><?php echo $cont['houses_totally_destroyed_num'] ?></h4>
+					        <h4><?php echo isset($cont['houses_totally_destroyed_num']) ?  $cont['houses_totally_destroyed_num'] : '' ?></h4>
 					        <p><?php echo $cont['houses_totally_destroyed_text'] ?></p>
 					      </li>
 					      <li style="margin-bottom: 20px">
-					        <h4><?php echo $cont['houses_burnt_num'] ?></h4>
+					        <h4><?php echo isset($cont['houses_burnt_num']) ? $cont['houses_burnt_num'] : '' ?></h4>
 					        <p><?php echo $cont['houses_burnt_text'] ?></p>
 					      </li>
 					      <li style="margin-bottom: 20px">
-					        <h4><?php echo $cont['houses_partially_damaged_num'] ?></h4>
+					        <h4><?php echo isset($cont['houses_partially_damaged_num']) ? $cont['houses_partially_damaged_num'] : '' ?></h4>
 					        <p><?php echo $cont['houses_partially_damaged_text'] ?></p>
 					      </li>
 					    </ul>
