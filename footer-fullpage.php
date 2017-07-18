8d7f6de7bf6e28255c66a1d7baa10a4a4753a927
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-17 20:52:36 -0500

    fix folder

--- footer-fullpage.php
+++ footer-fullpage.php
@@ -8,9 +8,9 @@
 <script src="<?php echo get_template_directory_uri() ?>/client/dist/app.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/app.js') ?>
 "></script>
 <script
-  src="<?php echo get_template_directory_uri() ?>/client/dist/vendor_me.js?v=<?php echo filemtime(get_template_directory() . '/client/me/vendor_me.js') ?>"></script>
+  src="<?php echo get_template_directory_uri() ?>/client/dist/vendor_me.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/vendor_me.js') ?>"></script>
 <script
-  src="<?php echo get_template_directory_uri() ?>/client/dist/app_me.js?v=<?php echo filemtime(get_template_directory() . '/client/me/app_me.js') ?>"></script>
+  src="<?php echo get_template_directory_uri() ?>/client/dist/app_me.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/app_me.js') ?>"></script>
 
 </body>
 </html>
