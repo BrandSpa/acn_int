d6451cfa41b71796bf7d545d1a34b3113dbd603e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-19 16:11:25 -0500

    close call

--- client/fullpage/slide_post.js
+++ client/fullpage/slide_post.js
@@ -15,9 +15,11 @@ function slidePost($fp) {
     $("body").addClass("scroll-stoped");
     $postSection.addClass("section__post--open");
     $(".section__close-post").addClass("section__close-post--open");
-    
+
     // emmiter.off("close:all", closePost.bind(this, $postSection));
-    emmiter.on("close:all", closePost.bind(this, $postSection));
+    emmiter.on("close:all", () => {
+      closePost.call(this, $postSection);
+    });
   }
 
   function handleTogglePost(e) {
