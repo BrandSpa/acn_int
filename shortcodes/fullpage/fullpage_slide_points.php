d0542bfb787f49acb08f550374e0ffd2afdab4f7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-19 15:30:44 -0500

    button outside

--- shortcodes/fullpage/fullpage_slide_points.php
+++ shortcodes/fullpage/fullpage_slide_points.php
@@ -27,8 +27,6 @@ function acn_fullpage_slide_points_sc( $atts, $content ) {
 
 	<div class="section__content">
 		<?php echo do_shortcode($content) ?>
-
-		<button class="section__open section__close-spot-content"> <i class="ion-close-round"></i> </button>
 		<div class="map-points__container">
 			<svg class="map-points" width="1409" height="695" preserveAspectRatio="xMidYMid slice" viewBox="150 0 1600 1000">
 				<image class="hotspot__bg-image" width="1920" height="1080"
@@ -191,7 +189,8 @@ function acn_fullpage_slide_points_sc( $atts, $content ) {
 </div>
 
 </div>
-
+	<button class="section__open section__close-spot-content"> <i class="ion-close-round"></i> </button>
+	
 	<?php foreach($contents as $cont): ?>
 		<?php require('points_content.php') ?>
 	<?php endforeach; ?>
