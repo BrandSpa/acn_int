33635271ef726af720eb3e82de5633d9af73491c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-21 18:20:11 -0500

    change order data

--- shortcodes/fullpage/points_content.php
+++ shortcodes/fullpage/points_content.php
@@ -16,24 +16,27 @@
 					      <p><?php echo $cont['families_already_returned_text'] ?></p>
 					    </div>
 
-							<div style="margin-bottom: 40px">
-					      <h3 style="color: red;font-weight: 500;"><?php echo isset($cont['damaged_houses_num']) ?  $cont['damaged_houses_num'] : ''?></h3>
-					      <h3 style="color: red;font-weight: 500;"><?php echo $cont['damaged_houses_title_1'] ?></h3>
-					      <h3 style="color: red;font-weight: 500;"><?php echo $cont['damaged_houses_title_2'] ?></h3>
-					    </div>
+							<div>
+								<h3><?php echo isset($cont['christians_already_returned_num']) ? $cont['christians_already_returned_num'] : ''?></h3>
+								<p><?php echo $cont['christians_already_returned_text'] ?></p>
+							</div>
 
-							<div style="margin-bottom: 40px">
-					    	<h1 style="font-weight: 700;"><?php echo isset($cont['percentage']) ? $cont['percentage'] : '' ?></h1>
-					    </div>
-
-					    <h4 style="color: red;"><?php echo isset($cont['number_of_houses_registered_to_be_renovated_num']) ? $cont['number_of_houses_registered_to_be_renovated_num'] : ''  ?></h4>
-					    <p><?php echo $cont['number_of_houses_registered_to_be_renovated_text'] ?></p>
 					  </div>
 
 					  <div class="col-4-l" style="padding-bottom: 40px">
 					    <div style="margin-bottom: 140px">
-						    <h3><?php echo isset($cont['christians_already_returned_num']) ? $cont['christians_already_returned_num'] : ''?></h3>
-						    <p><?php echo $cont['christians_already_returned_text'] ?></p>
+								<div style="margin-bottom: 40px">
+									<h3 style="color: red;font-weight: 500;"><?php echo isset($cont['damaged_houses_num']) ?  $cont['damaged_houses_num'] : ''?></h3>
+									<h3 style="color: red;font-weight: 500;"><?php echo $cont['damaged_houses_title_1'] ?></h3>
+									<h3 style="color: red;font-weight: 500;"><?php echo $cont['damaged_houses_title_2'] ?></h3>
+								</div>
+	<!--
+								<div style="margin-bottom: 40px">
+									<h1 style="font-weight: 700;"><?php echo isset($cont['percentage']) ? $cont['percentage'] : '' ?></h1>
+								</div> -->
+
+								<h4 style="color: red;"><?php echo isset($cont['number_of_houses_registered_to_be_renovated_num']) ? $cont['number_of_houses_registered_to_be_renovated_num'] : ''  ?></h4>
+								<p><?php echo $cont['number_of_houses_registered_to_be_renovated_text'] ?></p>
 					    </div>
 
 					    <ul style="margin-bottom: 40px">
