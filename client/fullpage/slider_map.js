6feca2adf1e71f975d6c4c6d0e36f952b45db2f3
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-24 11:52:40 -0500

    debug open NinevehGeneralContent

--- client/fullpage/slider_map.js
+++ client/fullpage/slider_map.js
@@ -40,7 +40,7 @@ function mapPoints($fp) {
   }
 
   function openNinevehGeneralContent(e) {
-    console.log($fp);
+    console.log("openNinevehGeneralContent");
     ScrollStop($fp, true);
     $(".section__close-nineveh-general").addClass("section__close-nineveh-general--open");
     $(".nineveh-general-content").addClass("nineveh-general-content--open")
