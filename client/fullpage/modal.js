4d5a3757e1f79d734d2ebaa27f4012dba9ca825a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-24 17:09:26 -0500

    add scroll stop

---
+++ client/fullpage/modal.js
@@ -0,0 +1,25 @@
+import { ScrollStop } from "./funs";
+
+function modal($fp) {
+  const emmiter = window.mitt;
+
+  function openModal(e) {
+    if(e) e.preventDefault();
+    ScrollStop($fp, true);
+    let modalName = $(this).attr("href").replace("#", "");
+    let $modal = $(`.section__modal[data-modal="${modalName}"]`);
+    $modal.addClass("section__modal--open");
+  }
+
+  function closeModal() {
+    if($(".section__modal--open").length > 0) {
+      ScrollStop($fp, false);
+      let $modal = $(".section__modal--open");
+      $modal.removeClass("section__modal--open");
+    }
+  }
+
+  $(".open-modal").on("click", openModal);
+  emmiter.on("close:esc", closeModal);
+
+}
