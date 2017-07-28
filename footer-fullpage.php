91479247d1b3f31666746ece6815ce2b9670ec6c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-28 11:44:02 -0500

    remove app.js

--- footer-fullpage.php
+++ footer-fullpage.php
@@ -4,9 +4,6 @@
   bs.donate = '<?php echo gett('Donate') ?>';
 </script>
 
-<script src="<?php echo get_template_directory_uri() ?>/client/dist/vendor.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/vendor.js') ?>"></script>
-<script src="<?php echo get_template_directory_uri() ?>/client/dist/app.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/app.js') ?>
-"></script>
 <script
   src="<?php echo get_template_directory_uri() ?>/client/dist/vendor_me.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/vendor_me.js') ?>"></script>
 <script
