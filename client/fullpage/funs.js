b0aeb5c2e88f88814b98d583974fb577a5891fbb
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-28 14:58:11 -0500

    stop keyboard events

--- client/fullpage/funs.js
+++ client/fullpage/funs.js
@@ -4,9 +4,11 @@ export const ScrollStop = ($fp, stop = false) => {
   console.log(stop);
   if(stop) {
     $fp.setAllowScrolling(false);
+    $fp.setKeyboardScrolling(false);
     $("body").addClass("scroll-stoped");
   } else {
     $fp.setAllowScrolling(true);
+    $fp.setKeyboardScrolling(true);
     $("body").removeClass("scroll-stoped");
   }
 }
