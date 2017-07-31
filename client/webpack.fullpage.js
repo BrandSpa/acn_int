95954dfa89f20d9e0707a4783287615acc84f296
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-31 14:48:05 -0500

    move polyfills and shims to vendor

--- client/webpack.fullpage.js
+++ client/webpack.fullpage.js
@@ -1,38 +1,40 @@
-'use strict';
-const webpack = require('webpack');
-const fs = require('fs');
-const Path = require('path');
-const WebpackCleanupPlugin =  require('webpack-cleanup-plugin');
+"use strict";
+const webpack = require("webpack");
+const fs = require("fs");
+const Path = require("path");
+const WebpackCleanupPlugin =  require("webpack-cleanup-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
     vendor: [
-      'jquery',
-      'mitt',
-			'fullpage.js',
-			'lazysizes'
+      "jquery",
+      "mitt",
+			"fullpage.js",
+			"lazysizes",
+      "babel-polyfill",
+      "airbnb-browser-shims"
     ],
-  	app_me: './fullpage/index.js'
+  	app_me: "./fullpage/index.js"
   },
   output: {
-  	path: Path.join(__dirname, '/dist/'),
-    filename: '[name].js'
+  	path: Path.join(__dirname, "/dist/"),
+    filename: "[name].js"
   },
   module: {
   	loaders: [
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader?cacheDirectory=true'
+				loader: "babel-loader?cacheDirectory=true"
 			}
 		]
   },
 	plugins: [
       new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        filename: 'vendor_me.js',
-        minChunks: 2
+        name: "vendor",
+        filename: "vendor_me.js",
+        minChunks: Infinity
       })
     ]
 };
