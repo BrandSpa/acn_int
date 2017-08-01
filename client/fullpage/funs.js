3adbccb4caf9a00a0acd7d4071088a7d5a921b9b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-01 10:51:27 -0500

    check property

--- client/fullpage/funs.js
+++ client/fullpage/funs.js
@@ -1,14 +1,16 @@
 import $ from "jquery";
 
 export const ScrollStop = ($fp, stop = false) => {
-  console.log(stop);
-  if(stop) {
-    $fp.setAllowScrolling(false);
-    $fp.setKeyboardScrolling(false);
-    $("body").addClass("scroll-stoped");
-  } else {
-    $fp.setAllowScrolling(true);
-    $fp.setKeyboardScrolling(true);
-    $("body").removeClass("scroll-stoped");
+  console.log($fp.hasOwnProperty("setAllowScrolling"));
+  if($fp.hasOwnProperty("setAllowScrolling")) {
+    if(stop) {
+      $fp.setAllowScrolling(false);
+      $fp.setKeyboardScrolling(false);
+      $("body").addClass("scroll-stoped");
+    } else {
+      $fp.setAllowScrolling(true);
+      $fp.setKeyboardScrolling(true);
+      $("body").removeClass("scroll-stoped");
+    }
   }
 }
