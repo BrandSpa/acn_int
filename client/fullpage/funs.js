77570b941ba21565f79f3822aa9ee0b0d0f80c4f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-01 11:22:41 -0500

    setAllowScrolling akk

--- client/fullpage/funs.js
+++ client/fullpage/funs.js
@@ -2,9 +2,10 @@ export const ScrollStop = ($fp, stop = false) => {
   console.log($fp.hasOwnProperty("setAllowScrolling"));
   if($fp.hasOwnProperty("setAllowScrolling")) {
     if(stop) {
-      $fp.setAllowScrolling(false);
+      $fp.setAllowScrolling(false, "all");
       $fp.setKeyboardScrolling(false);
       $("body").addClass("scroll-stoped");
+      return false;
     } else {
       $fp.setAllowScrolling(true);
       $fp.setKeyboardScrolling(true);
