a721831a41d356c1e1b71bb033051cd1e7274a61
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-01 11:40:36 -0500

    log events

--- client/fullpage/funs.js
+++ client/fullpage/funs.js
@@ -2,6 +2,7 @@ const emmiter = window.mitt;
 
 export const stopScroll = ($fp) => {
   emmiter.on("stop:scroll", function() {
+    console.log('stop');
     $fp.setAllowScrolling(false);
     $fp.setKeyboardScrolling(false);
     $("body").addClass("scroll-stoped");
@@ -10,6 +11,7 @@ export const stopScroll = ($fp) => {
 
 export const allowScroll = ($fp) => {
   emmiter.on("allow:scroll", function() {
+    console.log('allow');
     $fp.setAllowScrolling(true);
     $fp.setKeyboardScrolling(true);
     $("body").removeClass("scroll-stoped");
