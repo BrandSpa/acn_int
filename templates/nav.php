54dc5f7cd1151c2b6e729698fb747241f71b85e9
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-09 14:39:32 -0500

    fix err

--- templates/nav.php
+++ templates/nav.php
@@ -14,9 +14,10 @@
 		</a>
 
 		<?php
-		function getNavArr() {
-			$menuId = get_nav_menu_locations()['header'];
-			if($menuId) {
+		function getNavArr($locationName) {
+
+			if(isset(get_nav_menu_locations()[$locationName])) {
+				$menuId = get_nav_menu_locations()[$locationName];
 				return json_encode(wp_get_nav_menu_items($menuId));
 			} else {
 				return json_encode(array());
@@ -26,7 +27,7 @@
 
 		?>
 
-	<div class="bs-menu" data-props='{"links": <?php echo getNavArr() ?>}'></div>
+	<div class="bs-menu" data-props='{"links": <?php echo getNavArr("header") ?>}'></div>
 
 
 	<ul class="menu--mobile">
