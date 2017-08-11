2a3c776e2d03c3e1a3516c172d04dddb0cc56fc5
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-11 11:55:50 -0500

    change size card type

--- client/components/donate/four.js
+++ client/components/donate/four.js
@@ -52,7 +52,7 @@ class Four extends Component {
 		return (
 			<div style={this.props.show_four ? { display: "block", textAlign: "center" } : { display: "none" }}>
 				<h5 style={{fontSize: "18px", color: "#3C515F", marginBottom: "20px"}}>{texts.text_four_step}</h5>
-				<h3 style={{ color: "#3C515F", marginBottom: "20px" }}>{`${texts.subtext_four_step} $${d} USD`}</h3>
+				<h3 style={{ color: "#3C515F", marginBottom: "20px" }}>{`${texts.subtext_four_step} ${d} ?`}</h3>
 				<div className="col-6 col-6-l">
 					<button
 						onClick={this.handleYes}
