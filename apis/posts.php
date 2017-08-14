bdda25871a9f3af42424cfe6e1032522a8a5ffe5
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-14 17:00:10 -0500

    clean post content

--- apis/posts.php
+++ apis/posts.php
@@ -14,6 +14,7 @@ function bs_get_posts($type = array('video','gallery','featured','post'), $paged
  			$post->post_image = str_replace('http:', '', $images);
 			$content = substr($post->post_content, 0, 250) ? substr($post->post_content, 0, 250) : $post->post_content;
 			$post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags(substr($post->post_content, 0, 200)) );
+			$post->post_content = '';
 			$post->post_permalink = get_post_permalink($post->ID);
 			return $post;
 	}, $query->get_posts());
