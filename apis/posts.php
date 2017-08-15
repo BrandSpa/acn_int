9697ac696fafccaaefa50f3531685abec73c0c2e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-15 10:44:00 -0500

    clean content option api posts

--- apis/posts.php
+++ apis/posts.php
@@ -5,7 +5,8 @@ function bs_get_posts(
 	$paged = 0,
 	$category = '',
 	$perpage = '6',
-	$status) {
+	$status,
+	$cleanContent) {
 
 	$query = new Wp_Query(array(
     'post_type' => $type,
@@ -20,8 +21,11 @@ function bs_get_posts(
  			$post->post_image = str_replace('http:', '', $images);
 			$content = substr($post->post_content, 0, 250) ? substr($post->post_content, 0, 250) : $post->post_content;
 			$post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags(substr($post->post_content, 0, 200)) );
-			$post->post_content = '';
 			$post->post_permalink = get_post_permalink($post->ID);
+			
+			if($cleanContent) {
+				$post->post_content = '';
+			}
 			return $post;
 	}, $query->get_posts());
 
@@ -34,8 +38,8 @@ function wp_get_posts() {
   $category = isset($_POST['post_category']) ? $_POST['post_category'] : '';
   $perpage = isset($_POST['post_perpage']) ? $_POST['post_perpage'] : '6';
   $status = isset($_POST['post_status']) ? $_POST['post_status'] : 'publish';
-	$cleanContent = isset($_POST['post_clean_content']) ? $_POST['post_clean_content'] : true;
-  $res = bs_get_posts($post_type, $paged, $category, $perpage, $status);
+	$cleanContent = isset($_POST['post_clean_content']) ? false : true;
+  $res = bs_get_posts($post_type, $paged, $category, $perpage, $status, $cleanContent);
 
   responseJson($res);
   die();
