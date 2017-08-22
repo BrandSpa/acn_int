c4a8952f8675cad88138fcdb341ef9736c67b49a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-22 12:30:46 -0500

    redirect

--- lib/offices_countries.php
+++ lib/offices_countries.php
@@ -1,31 +1,31 @@
 <?php
 
 function getOfficesCountries() {
-  return  array(
-  "default",
-  "Australia" ,
-  "Austria" ,
-  "Belgium" ,
-  "Brazil" ,
-  "Canada" ,
-  "Chile" ,
-  "Colombia" ,
-  "France" ,
-  "Germany" ,
-  "Ireland" ,
-  "Italy" ,
-  "Malta" ,
-  "Mexico" ,
-  "Netherlands" ,
-  "Philippines" ,
-  "Poland" ,
-  "Portugal" ,
-  "Spain" ,
-  "Republic of Korea" ,
-  "Switzerland" ,
-  "United Kingdom",
-  "United States"
-);
+  return  [
+    "default",
+    "Australia",
+    "Austria",
+    "Belgium",
+    "Brazil",
+    "Canada",
+    "Chile",
+    "Colombia",
+    "France",
+    "Germany",
+    "Ireland",
+    "Italy",
+    "Malta",
+    "Mexico",
+    "Netherlands",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Spain",
+    "Republic of Korea",
+    "Switzerland",
+    "United Kingdom",
+    "United States"
+  ];
 
 }
 
