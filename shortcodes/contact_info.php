78fa04a460fdf586beff7597380c79e8df1b2816
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-23 10:23:12 -0500

    add name

--- shortcodes/contact_info.php
+++ shortcodes/contact_info.php
@@ -6,11 +6,12 @@ function bs_contact_info_sc($atts, $content = null) {
 	ob_start();
 ?>
 
-<?php 
-	if(in_array($country, getOfficesCountries())): 
+<?php
+	if(in_array($country, getOfficesCountries())):
 	$country = space_to_lodash($country);
 ?>
 	<div class="bs-contact-info">
+		<h5><?php echo get_option('contact_info_name_' . $country) ?></h5>
 		<h6><?php echo get_option('contact_info_address_' . $country) ?></h6>
 		<h6><?php echo get_option('contact_info_email_' . $country) ?></h6>
 		<h6><?php echo get_option('contact_info_phone_' . $country) ?></h6>
