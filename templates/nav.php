d70e23e095f4b658bae603465d06a6edf89d01eb
Alejandro Sanabria <alesanabriav@gmail.com>
2017-09-12 16:05:32 -0500

    nav array

--- templates/nav.php
+++ templates/nav.php
@@ -13,21 +13,6 @@
 			</svg>
 		</a>
 
-		<?php
-		function getNavArr($locationName) {
-
-			if(isset(get_nav_menu_locations()[$locationName])) {
-				$menuId = get_nav_menu_locations()[$locationName];
-				return json_encode(wp_get_nav_menu_items($menuId));
-			} else {
-				return json_encode(array());
-			}
-		}
-
-
-		?>
-
-	<!-- <div class="bs-menu" data-props='{"links": <?php echo getNavArr("header") ?>}'></div> -->
 
 	<ul class="menu">
 		<?php
