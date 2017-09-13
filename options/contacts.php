cb8359c2f1a6f6fd948a8f4cf72cfe077fcef700
Alejandro Sanabria <alesanabriav@gmail.com>
2017-09-13 16:10:13 -0500

    replace

--- options/contacts.php
+++ options/contacts.php
@@ -44,7 +44,7 @@ function bs_contacts_options() {
     </thead>
     <tbody>
       <?php foreach($posts as $post): ?>
-        <?php $contact = json_decode(str_replace('\/', '', $post->post_content)); ?>
+        <?php $contact = json_decode(str_replace("\\", '', $post->post_content)); ?>
         <tr>
           <td><?php echo $contact->email; ?></td>
           <td><?php echo $contact->name; ?></td>
