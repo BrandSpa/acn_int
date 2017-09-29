6461388f3da584f56e4208b2066ed922da785773
Alejandro Sanabria <alesanabriav@gmail.com>
2017-09-29 11:45:01 -0500

    add continents translations

--- translations/countries.php
+++ translations/countries.php
@@ -4,3 +4,7 @@ include_once str_replace('translations', '', __DIR__) . 'lib/countries.php';
 foreach(getCountries() as $key => $trans) {
 	register_translation('bs-' . $key, $trans, 'BS countries');
 }
+
+foreach(getContinentsList() as $key => $continent) {
+	register_translation('bs-' . $key, $continent, 'BS countinents');
+}
