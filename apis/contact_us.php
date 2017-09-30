e406efa45a77da0b1dd3c81175cbba881c821996
Alejandro Sanabria <alesanabriav@gmail.com>
2017-09-30 14:47:36 -0500

    send mail

--- apis/contact_us.php
+++ apis/contact_us.php
@@ -38,11 +38,12 @@ function contact_us($data = [], $smtp) {
 
     $result = wp_insert_post($postarr);
 
-    return $result;
+    if($result) {
+      return sendMail($data, $smtp);
+    }
   }
-  if($result) {
-    return sendMail($data, $smtp);
-  }
+
+
 
   return 'Insert post error';
 }
