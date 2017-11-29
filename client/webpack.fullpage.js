882b5ae94cab700bf8fa15f6eca4c56f8f4b747d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-11-28 21:50:04 -0500

    compile fp

--- client/webpack.fullpage.js
+++ client/webpack.fullpage.js
@@ -2,8 +2,6 @@
 const webpack = require("webpack");
 const fs = require("fs");
 const Path = require("path");
-const WebpackCleanupPlugin =  require("webpack-cleanup-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -11,12 +9,11 @@ module.exports = {
       "mitt",
 			"fullpage.js",
 			"lazysizes",
-      "airbnb-browser-shims"
     ],
-  	app_me: "./fullpage/index.js"
+  	app_me: "./src/fullpage/index.js"
   },
   output: {
-  	path: Path.join(__dirname, "/dist/"),
+  	path: Path.join(__dirname, '/dist/'),
     filename: "[name].js"
   },
   module: {
