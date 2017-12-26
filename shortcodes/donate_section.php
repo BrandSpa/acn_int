2791c04986db66f60d756d396eb969f118513328
wrk <wrk@MacBook-Air-de-wrk.local>
2017-12-26 16:18:06 -0500

    add prefixes

--- shortcodes/donate_section.php
+++ shortcodes/donate_section.php
@@ -52,7 +52,8 @@ function bs_donate_section_sc($atts, $content = null) {
 
 	 $props = [
  		"texts" => $at,
- 		"countries" => function_exists('getCountries') ? getCountries() : [],
+		"countries" => function_exists('getCountries') ? getCountries() : [],
+		"prefixes" => function_exists('country_code') ? country_code() : [],
  		"is_blue" => $at['is_blue'],
  		"redirect" => [
  			"monthly" => $at['donate_monthly_redirect'],
