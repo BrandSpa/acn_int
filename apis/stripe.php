bc7e7cc3adbc238e1c6c55ed4696a407622ed130
wrk <wrk@MacBook-Air-de-wrk.local>
2017-12-27 10:22:10 -0500

    stripe debug

--- apis/stripe.php
+++ apis/stripe.php
@@ -126,6 +126,7 @@ function get_plan_name($amount) {
 
 function stripe_once($api_key, $data) {
   $customer = stripe_create_customer($api_key, $data);
+  print_r($customer);
   $data['customer'] = $customer->id;
   return stripe_create_charge($api_key, $data);
 }
