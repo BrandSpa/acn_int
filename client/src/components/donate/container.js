3c83b404e4066989fe607252b6c3d95acfc3510a
Michael Sanchez <michaelsanchez@brandspa.com>
2018-01-05 14:46:25 -0500

    ajustes

--- client/src/components/donate/container.js
+++ client/src/components/donate/container.js
@@ -19,35 +19,7 @@ class DonateContainer extends Component {
     const { texts } = this.props;
     console.log('container')
     return (
-      <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', 'justify-content': 'center' }}>
-        <div className="col-12-l" style={{ margin: '40px 0' }}>
-          <h2
-            style={{
-              color: '#fff',
-              textAlign: 'center',
-              textShadow: '0 2px 20px rgba(0,0,0,0.47)',
-            }}
-          >
-            {this.state.section == 0 ? texts.title : texts.success_title}
-          </h2>
-          <h3
-            style={{
-              color: '#fff',
-              textAlign: 'center',
-              textShadow: '0 2px 20px rgba(0,0,0,0.47)',
-              fontWeight: '300',
-            }}
-          >
-            {this.state.section == 0 ? texts.subtitle : texts.success_subtitle}
-          </h3>
-        </div>
-        <div
-          className={'col-12 col-4-l'}
-          style={{ background: '#fff', padding: '40px' }}
-        >
-          <Donate {...this.props} changeSection={this.changeSection} />
-        </div>
-      </div>
+        <Donate {...this.props} changeSection={this.changeSection} />
     );
   }
 }
