5c7da0b456bc5383216a6d82ebcdf3c43437442e
Michael Sanchez <michaelsanchez@brandspa.com>
2018-01-10 14:39:01 -0500

    chage to list

--- search.php
+++ search.php
@@ -2,9 +2,10 @@
 
 <div id="acn_int" class="l-wrap" >
   <?php get_search_form(); ?>
+  <ul>
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <div class="search-result">
+    <li class="search-result">
         <h3 class="card-header">
             <a href="<?php the_permalink() ?>"><?php the_title() ?>  </a>
             <span class="badge"><?php echo get_post_type() ?></span>
@@ -15,13 +16,14 @@
         if($posttype !== 'page'): ?>
         <div class="search-result-resume"><?php the_content() ?></div>
         <?php endif; ?>
-    </div>
+    </li>
     
   <?php endwhile; else : ?>
     <h1>
     <?php echo "no results found" ?>
     </h1>
   <?php endif; ?>
+  </ul>
 </div>
 
 <?php get_footer() ?>
