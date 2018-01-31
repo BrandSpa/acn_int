5876ad54ab8140a844fdfe9277702a7c5b903f7b
mfsanchez <m1ch43ls4nch3z@gmail.com>
2018-01-31 15:24:46 -0500

    add container

--- search.php
+++ search.php
@@ -1,39 +1,41 @@
 <?php get_header() ?>
+<div class="search-container">
+    <div id="acn_int" class="l-wrap" >
+    <ul class="search-results">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<div id="acn_int" class="l-wrap search-container" >
-  <ul class="search-results">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <li class="bs-posts-list__item">
 
-    <li class="bs-posts-list__item">
-
-        <?php if(get_the_post_thumbnail_url() || get_option('logo')): ?>
-            <a href="<?php the_permalink() ?>">
-            <div 
-                class="bs-posts-list__item__img" 
-                style="background-image: url(<?php echo get_the_post_thumbnail_url()?:get_option('logo') ?>);background-size: 100% auto; background-repeat: no-repeat;">
-            </div>
-            </a>
-        <?php endif; ?>
-        <div class="bs-posts-list__item__content">
-            <h2 class="card-header">
-                <a href="<?php the_permalink() ?>"><?php the_title() ?>  </a>
-                <span class="badge"><?php echo get_post_type() ?></span>
-            </h2>
-            <?php 
-            $posttype = get_post_type();    
-            if($posttype !== 'page'): ?>
-            <div class="resume"><p><?php the_excerpt() ?></p></div>
+            <?php if(get_the_post_thumbnail_url() || get_option('logo')): ?>
+                <a href="<?php the_permalink() ?>">
+                <div 
+                    class="bs-posts-list__item__img" 
+                    style="background-image: url(<?php echo get_the_post_thumbnail_url()?:get_option('logo') ?>);background-size: 100% auto; background-repeat: no-repeat;">
+                </div>
+                </a>
             <?php endif; ?>
-        </div>
+            <div class="bs-posts-list__item__content">
+                <h2 class="card-header">
+                    <a href="<?php the_permalink() ?>"><?php the_title() ?>  </a>
+                    <span class="badge"><?php echo get_post_type() ?></span>
+                </h2>
+                <?php 
+                $posttype = get_post_type();    
+                if($posttype !== 'page'): ?>
+                <div class="resume"><p><?php the_excerpt() ?></p></div>
+                <?php endif; ?>
+            </div>
+            
+        </li>
         
-    </li>
-    
-  <?php endwhile; else : ?>
-    <h3 class="search-empty-message">
-    <?php echo "<span class='bold-message'>". get_search_query() ."</span> ". __("did not match any post") ?>
-    </h3>
-  <?php endif; ?>
-  </ul>
+    <?php endwhile; else : ?>
+        <h3 class="search-empty-message">
+        <?php echo "<span class='bold-message'>". get_search_query() ."</span> ". __("did not match any post") ?>
+        </h3>
+    <?php endif; ?>
+    </ul>
+    </div>
 </div>
+
 
 <?php get_footer() ?>
